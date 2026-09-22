649bdea3311acc142a1e6373d94ab5bd890f31ad
NQD190523 <duc19052003@gmail.com>
2026-09-23 01:11:14 +0700

    apply thread pool technique to handle incom connection

--- main.go
+++ main.go
@@ -30,13 +30,21 @@ func main() {
 
 	fmt.Println("Server listening on port 8080")
 
+	threadPoolSize := 5                        // Set the desired thread pool size
+	sem := make(chan struct{}, threadPoolSize) // Create a semaphore channel to limit the number of concurrent goroutines
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection:", err)
 			continue
 		}
-		handleConnection(conn)
+		// go handleConnection(conn) // 1 connect equals 1 goroutine, so we can handle multiple connections concurrently
+		sem <- struct{}{} // Acquire a slot in the semaphore
+		go func() {
+			handleConnection(conn)
+			<-sem // Release the slot in the semaphore
+		}()
 	}
 
 }
